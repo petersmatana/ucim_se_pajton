98a0dc3af25dc90810e794a8e9cc41f9932e20b2
Peter Smatana <petersmatana@gmail.com>
2017-02-08 12:57:57 +0100

    hraju si s unicode

--- regexp.py
+++ regexp.py
@@ -9,6 +9,8 @@ taky se hodí vyhledávat nějakou
 diakritiku ŽLUŤOUČKÉHO koně a jiná
 zviřátka :)"""
 
+text2 = u'v tomto teřxtu budu mega moc hleřdat a hledat a hledat, jeřstli nenajdu taky se hořdí vyhledávat nějařkou diakritiku ŽLUŤOUřČKÉHO koně a jiřná zviřátka :)'
+
 
 def pokus1():
     match_obj = re.match(r'ŽLUŤOUČKÉHO', text)
@@ -19,11 +21,20 @@ def pokus1():
 
 
 def pokus2():
-    search_obj = re.search(r'ŽLUŤOUČKÉHO', text)
+    search_obj = re.search(r'ŽLUŤoUČKÉHO', text)
+
+    """
+    jinymi slovy pokud search_obj klapne tak vraci objekt
+    Match. pokud nenajde, vraci None
+    """
+    # print 'typ search_obj = ', type(search_obj)
+
     if search_obj:
         print '2) search, je tu'
     else:
         print '2) search, neni tu'
+
+# pokus2()
 
 
 def pokus3():
@@ -34,4 +45,16 @@ def pokus3():
     else:
         print '3) match, neni tu'
 
-pokus3()
+
+def get_char():
+    return u'ř'
+
+
+def unicode_pokus1():
+    reg_exp = re.search(get_char(), text2, re.U)
+    if reg_exp:
+        print 'jo'
+    else:
+        print 'ne'
+
+unicode_pokus1()
